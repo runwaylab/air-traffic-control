f0949f34ae22b8cdf94e70108e90f610c0763de7
grantbirki <grantbirki@github.com>
2023-04-04 17:45:11 +0100

    markdown link works

--- fixtures/docker/database/data.sql
+++ fixtures/docker/database/data.sql
@@ -27,7 +27,7 @@ CREATE TABLE commands (
 INSERT INTO commands(id, organization, repository, name, data) VALUES
 ('5ecfdb3a-c229-4982-b5b0-5cc87b8a616a', 'runwayapp', 'test-flight', 'deploy command', '{"name": "deploy command", "description": "Deploy the application", "command": ".deploy"}'),
 ('8ff93daa-66dc-4398-9ad7-93a480ac8ad7', 'runwayapp', 'test-flight', 'linter', '{"name": "linter", "description": "it lints things", "command": ".lint"}'),
-('33393daa-66dc-4398-9ad7-93a480ac8333', 'runwayapp', 'test-flight', 'test command', '{"name": "test command", "description": "triggers an Actions workflow and leaves a comment", "command": ".test", "actions": [{"type": "comment", "text": "I am starting the .github/workflows/test.yml workflow via a dispatch"}, {"type": "workflow_dispatch", "path": "test.yml"}]}'),
+('33393daa-66dc-4398-9ad7-93a480ac8333', 'runwayapp', 'test-flight', 'test command', '{"name": "test command", "description": "triggers an Actions workflow and leaves a comment", "command": ".test", "actions": [{"type": "comment", "text": "I am starting the [.github/workflows/test.yml](https://github.com/runwayapp/test-flight/actions/workflows/test.yml) workflow via a dispatch"}, {"type": "workflow_dispatch", "path": "test.yml"}]}'),
 ('58890287-9ff4-4ffa-b671-05ac33b9372e', 'runwayapp', 'fake-repo', 'help', '{"name": "help", "description": "a general help command", "command": ".help"}'),
 ('5a253c4d-ae3f-4b8d-aabd-f418c34f1d1f', 'monalisa', 'cats', 'help', '{"name": "help", "description": "a general help command", "command": ".help"}'),
 ('e497b87c-7bc7-4565-8477-54c8f9441cd0', 'lisamona', 'dogs', 'help', '{"name": "help", "description": "a general help command", "command": ".help"}');
